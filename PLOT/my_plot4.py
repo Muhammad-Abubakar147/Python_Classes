86c864f9060be62c493845111833ecd7ae1145c5
Muhammad Abubakar <brandmirza702@gmail.com>
2026-04-11 14:54:41 +0500

    Added some things new

--- PLOT/my_plot4.py
+++ PLOT/my_plot4.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 Labels=["Pass","Fail"]
 revenue=[50,50]
-plt.pie(revenue,labels=Labels,autopct="%1.1f%%",shadow=True,colors=["skyblue","red"])
+plt.pie(revenue,labels=Labels,autopct="%1.1f%%",shadow=True,colors=["black","blue"])
 plt.title("Result of Matric student 2025")
 plt.show()                                    
 
