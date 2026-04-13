f35034b0a55918e42a319590de35173cdfec56d5
Muhammad Abubakar <brandmirza702@gmail.com>
2026-04-13 22:04:55 +0500

    Dictonary example 2

---
+++ __pycache__/Dictonary-2.py
@@ -0,0 +1,10 @@
+dict={
+    "name":["muhammad abubakar","muhammad ammar","muhammad afzal"],
+    "age":[21,18,44],
+    "marks":(763,555,554),
+    "city":"faisalabad",
+}
+dict ["name"]="muhammad saad"
+print(dict["name"])
+print(dict["age"])
+print(dict["marks"])
