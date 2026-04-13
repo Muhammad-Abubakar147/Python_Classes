2749912fe19395d72f515dfe49b62ede194a8210
Muhammad Abubakar <brandmirza702@gmail.com>
2026-04-13 21:15:28 +0500

    histogram

--- PLOT/my_plot6.py
+++ PLOT/my_plot6.py
@@ -1,3 +1,4 @@
+#histogram
 import matplotlib.pyplot as plt
 Students_score=[32,45,63,34,2,76,95,64,10,64,43,12,34,56,78,90,9,98,76,54,32,10,130,62,56,67,85,94,87,55,64,43,83,92,74,57,67,77,88,99,66,36,39]
 plt.hist(Students_score,bins=8,color="red",edgecolor="black")
