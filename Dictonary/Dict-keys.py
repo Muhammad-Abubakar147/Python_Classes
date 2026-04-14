ba92b6ad0c48d298717bce85267ed58fb455d559
Muhammad Abubakar <brandmirza702@gmail.com>
2026-04-14 23:09:53 +0500

    dict key method

---
+++ Dictonary/Dict-keys.py
@@ -0,0 +1,12 @@
+student={
+    "name":"muhammad abubaka",
+    "age":21,
+    "subjects":{
+        "phy":72,
+        "chem":42,
+        "bio":32,
+        "bioinformatics":32,
+    }
+
+}
+print(student.keys())
