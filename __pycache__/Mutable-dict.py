6f6115b74e41a39c3945d40f80a321380b6f8482
Muhammad Abubakar <brandmirza702@gmail.com>
2026-04-14 12:40:20 +0500

    Added example no 3 of Mutable dicT

--- __pycache__/Mutable-dict.py
+++ __pycache__/Mutable-dict.py
@@ -18,3 +18,19 @@ dict={
 }
 student["age"]=34,
 print(student["age"])
+
+# Example no 3 of Mutable dict
+
+student={
+    "first name":"Rahma",
+    "second name":"abubakar",
+    "third name":"maryam",
+    "fourth name":"emaaan fatima",
+    "fifth name":"malaika",
+    "six name":"khan muhammad",
+    "marks of rahma":888,
+
+}
+student["fifth name"]="khan muhammad"
+student["six name"]="malaika"
+print(student)
