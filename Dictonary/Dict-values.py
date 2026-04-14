87ad4420d6619a974ba24f0d56acf61564bfbd0a
Muhammad Abubakar <brandmirza702@gmail.com>
2026-04-14 23:11:30 +0500

    Dict values

---
+++ Dictonary/Dict-values.py
@@ -0,0 +1,10 @@
+student={
+    "name":"muhammad hammad youasaf",
+    "age":31,
+    "city":"faisalabad",
+    "siblings":{
+        "sisters":["hamna","natasha","fizaa"],
+        "brothers":"hammad",
+    }
+}
+print(student.values())
