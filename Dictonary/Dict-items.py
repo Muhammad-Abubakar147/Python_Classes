d6c90fa2df8c5c9fde7122d0e2438aa71c91b8c3
Muhammad Abubakar <brandmirza702@gmail.com>
2026-04-14 23:12:53 +0500

    Dict items

---
+++ Dictonary/Dict-items.py
@@ -0,0 +1,11 @@
+zoology={
+    "bs zoology":["rahma","abubakar","moiz","sehar"],
+    "bs math":["mahira","saana","junaid"],
+    "age":21,
+    "subjects":{
+        "phy":21,
+        "chem":32,
+        "bio":32,
+    }
+}
+print(zoology.items())
