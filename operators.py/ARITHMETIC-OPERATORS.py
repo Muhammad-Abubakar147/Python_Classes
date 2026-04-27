d2f989e8315ad0c4163bf31706d71c023f46b7f4
Muhammad Abubakar <brandmirza702@gmail.com>
2026-04-27 19:25:08 +0500

    Atrhmetic operators

---
+++ operators.py/ARITHMETIC-OPERATORS.py
@@ -0,0 +1,12 @@
+# ==============================
+# PYTHON OPERATORS COMPLETE LIST
+# ==============================
+
+# 1. ARITHMETIC OPERATORS
+a = 10
+b = 3
+
+print("Arithmetic Operators:")
+print("Addition:", a + b)        # 13
+print("Subtraction:", a - b)     # 7
+print("Multiplication:", a * b)  # 30
