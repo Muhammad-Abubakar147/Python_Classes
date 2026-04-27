457fd531ef913e3c94f546e8970dd0acce8b86a2
Muhammad Abubakar <brandmirza702@gmail.com>
2026-04-27 19:58:45 +0500

    Arithematic operators

--- operators.py/ARITHMETIC-OPERATORS.py
+++ operators.py/ARITHMETIC-OPERATORS.py
@@ -10,3 +10,7 @@ print("Arithmetic Operators:")
 print("Addition:", a + b)        # 13
 print("Subtraction:", a - b)     # 7
 print("Multiplication:", a * b)  # 30
+print("Division:", a / b)        # 3.33
+print("Floor Division:", a // b) # 3
+print("Modulus:", a % b)         # 1
+print("Exponent:", a ** b)       # 1000
