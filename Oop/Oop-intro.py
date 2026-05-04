77eb3e54156138b1a22752edc929e74e8c1eeb72
Muhammad Abubakar <brandmirza702@gmail.com>
2026-05-04 19:17:17 +0500

    oop intro

---
+++ Oop/Oop-intro.py
@@ -0,0 +1,12 @@
+# 🧠 OOP Introduction
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+# Creating object
+s1 = Student("Ali", 20)
+
+print("Name:", s1.name)
+print("Age:", s1.age)
