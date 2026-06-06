ae21fdc286b3c071fa780a5276489d4363b95416
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-06 14:52:46 +0500

    Function Ex 1

--- Function/Function-ex-1.py
+++ Function/Function-ex-1.py
@@ -1,8 +1,8 @@
 #Calculating average 
 
-def cal_average(a,b,c,d):
-    sum=a+b+c+d
-    average=sum/4
+def cal_average(a,b,c,d): #Parameters
+    sum=a+b+c+d #will print(sum)
+    average=sum/4 #will print(average)
     print(average)
     return(average)
-cal_average(199,677,556,990)
+cal_average(199,677,556,990) # will give output =>605.5
