b604d13b2dca1490497ddaf92eed07767ec5961a
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-06 12:35:06 +0500

    function example

--- Function/Syntax.py
+++ Function/Syntax.py
@@ -4,3 +4,8 @@ def function_name(parameters):
 #Here def is key word which will use for function
 #function_name is (which will name of function is)
 #parameters (which we will give in function for usage)
+
+def add (a,b):
+    print(a+b)
+    
+add(10,90)
