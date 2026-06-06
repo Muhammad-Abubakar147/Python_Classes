e4c599ec79d04fde484d9e13a67a37db7284e41b
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-06 22:33:57 +0500

    Built in Function in Python

--- Function/Built-in-function.py
+++ Function/Built-in-function.py
@@ -1,2 +1,10 @@
 #Built in function is pre define function in python
 #We can say that" A built-in function in Python is a pre-defined function provided by Python itself."
+
+print("Hello, World!")
+
+numbers = [10, 20, 30]
+
+print(len(numbers))      # 3
+print(max(numbers))      # 30
+print(sum(numbers))      # 60
