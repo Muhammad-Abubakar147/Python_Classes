1a7e4ccd4b3e9fe91146ef2eab440ab54170e800
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-09 20:03:07 +0500

    File I/O line by line

--- File I/O/Read.function.py
+++ File I/O/Read.function.py
@@ -5,6 +5,7 @@ print(data)
 f.close()
 
 #If we want to read any file line by line then we will use this syntax
+print("This syntax for printing line by line")
 f = open("File I\O\Lecture-file.txt", "r")
 line1 = f.readline()
 print(line1)
