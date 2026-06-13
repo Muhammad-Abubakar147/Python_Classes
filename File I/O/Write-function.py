7c1c0c64e659a4a5c9ee228a5457a3972c64156b
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-13 15:04:30 +0500

    File I/O

--- File I/O/Write-function.py
+++ File I/O/Write-function.py
@@ -1,10 +1,9 @@
-f=open("File I\O\File-Lecture.txt","r")
-data=f.read()
-print(data)
-f.close()
+# f=open("File I\O\File-Lecture.txt","r")
+# data=f.read()
+# print(data)
+# f.close()
 
 #For writting more
-f=open("File I\O\File-Lecture.txt","w")
-data=f.read()
-print(data)
+f= open ("File I\O\File-Lecture-for-editing.txt","w")
+f.write ("My work is appending new text")
 f.close()
