daf6bb389efce70b6292e3483f46f68b318090dd
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-25 14:56:28 +0500

    Slicing of tuple

--- Tuple/slicing-of-tuple.py
+++ Tuple/slicing-of-tuple.py
@@ -1,2 +1,5 @@
 tup=(1,2,3,4,5,5,77,8,9,9,7,6,5,4,43,) #tuple 
 print(tup[3]) #will print "4" bcz 4 is on 3rd number in tuple
+
+tup2=(1,2,3,4,5,5,77,8,9,9,7,6,5,4,43,) 
+tup2[3]=4 #this is not allowed in tuple 
