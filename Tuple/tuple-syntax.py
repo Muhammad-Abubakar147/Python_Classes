d9b3cdae421ea301bcba142e9e593f43d0d2c4f2
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-25 14:37:12 +0500

    Syntax of Tuple

--- Tuple/tuple-syntax.py
+++ Tuple/tuple-syntax.py
@@ -1,3 +1,3 @@
-tup=(1,2,3,4)
-print(tup)
-print(type(tup))
+tup=(1,2,3,4) #This is how we can make tuple
+print(tup) #it will print( 1,2,3,4)
+print(type(tup))#this will print type of tup which is => tuple
