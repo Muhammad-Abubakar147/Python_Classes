c4a711b480c20552d917371a49c367e88fda347b
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-25 15:03:38 +0500

    String in tuple

--- Tuple/string-in-tuple.py
+++ Tuple/string-in-tuple.py
@@ -1,3 +1,8 @@
 name=("Muhammad Abubakar") #it will suppose as a "string" not as a tuple
 print(name)
 print(type(name)) #<class 'str'>
+
+
+Name=("Muhammad Abubakar",) #it will suppose as a "tuple" 
+print(type(Name)) #<class 'tuple'>
+#This is just bcz of "," we make in Name variable not in name 
