9e767b549469a47525b65dc7f3c6260ffaf4bc17
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-25 15:20:04 +0500

    Question no 1

---
+++ List/Question in list/Question-no-1.py
@@ -0,0 +1,11 @@
+#Input 3 movies name and store them in a list
+
+movies=[]
+mov1=input("Enter name of movie 1 :")
+mov2=input("Enter name of movie 2 :")
+mov3=input("Enter name of movie 3 :")
+
+movies.append(mov1)
+movies.append(mov2)
+movies.append(mov3)
+print(movies)
