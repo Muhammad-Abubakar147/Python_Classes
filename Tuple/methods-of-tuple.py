2918c00e304162357dbde5ccfb9d3d421f914c2b
Muhammad Abubakar <brandmirza702@gmail.com>
2026-06-25 14:47:13 +0500

    Methods in Tuple

--- Tuple/methods-of-tuple.py
+++ Tuple/methods-of-tuple.py
@@ -3,6 +3,12 @@ tup=() #We can store empty tuple
 print(tup) 
 print(type(tup)) #Tuple
 
-
+#What is not Allowed in tuple
 tup2=(1,2,3,4,5,6,7.8,6)
 tup2[3]=3 #it is not allowed in tuple
+
+#types of tuple
+tup=() #print empty tuple
+tup2=(12,3,4,5)#print int present in it .
+tup3=(1,) #print(1,) "," which can also possible in tuple if uh can write after int uh can
+tup4=(1)  #if uh not want to write "," at the end its also possible in tuple
